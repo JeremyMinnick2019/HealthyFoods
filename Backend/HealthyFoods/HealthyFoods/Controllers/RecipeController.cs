fd640a3f210121566d354fde9c36d59e46604fb8
Jeremy <JeremyMinnick2019@Gmail.com>
2019-11-13 16:50:51 -0500

    controllers done

--- Backend/HealthyFoods/HealthyFoods/Controllers/RecipeController.cs
+++ Backend/HealthyFoods/HealthyFoods/Controllers/RecipeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HealthyFoods.Models;
+using HealthyFoods.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,51 @@ namespace HealthyFoods.Controllers
     [ApiController]
     public class RecipeController : ControllerBase
     {
+        private IRepository<Recipe> recipeRepo;
+
+        public RecipeController(IRepository<Recipe> recipeRepo)
+        {
+            this.recipeRepo = recipeRepo;
+        }
+
+        // GET api/Albums
+        [HttpGet]
+        public IEnumerable<Recipe> Get()
+        {
+            return recipeRepo.GetAll();
+        }
+
+        // GET api/Albums/5
+        [HttpGet("{id}")]
+        public Recipe Get(int id)
+        {
+            return recipeRepo.GetById(id);
+        }
+
+        // POST api/Albums
+        [HttpPost]
+        public IEnumerable<Recipe> Post([FromBody] Recipe recipe)
+        {
+            recipeRepo.Create(recipe);
+            return recipeRepo.GetAll();
+
+        }
+
+        // PUT api/Albums/5
+        [HttpPut("{id}")]
+        public IEnumerable<Recipe> Put([FromBody] Recipe recipe)
+        {
+            recipeRepo.Update(recipe);
+            return recipeRepo.GetAll();
+        }
+
+        // DELETE api/Albums/5
+        [HttpDelete("{id}")]
+        public IEnumerable<Recipe> Delete(int id)
+        {
+            var deleteAlbum = recipeRepo.GetById(id);
+            recipeRepo.Delete(deleteAlbum);
+            return recipeRepo.GetAll();
+        }
     }
 }
