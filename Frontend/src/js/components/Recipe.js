aedcd1d9a066cbb46f0ff880ab5aa742b5918d4d
Aaron Rock <aaronrk@mail.com>
2019-11-14 13:24:12 -0500

    recipe components

---
+++ Frontend/src/js/components/Recipe.js
@@ -0,0 +1,21 @@
+export default function Recipe(recipe) {
+    return `
+
+<section id="Recipe-setup">    
+<ul> 
+    ${recipe
+      .map(recipePage => {
+        return `
+             <li class="recipe-list" id="recipe">
+                <h2>${recipePage.title}</h2>
+                <h4>${recipePage.calorie}</h4>
+                <p>${recipePage.instructions}</p>
+                <p><input class="mealtype_id" type="hidden" value="${recipePage.Id}"></p>
+                <p><input class="mealtype_id" type="hidden" value="${recipePage.MainIngredientId}"></p>
+             </li>
+        `;
+      })
+      .join("")}
+ </ul>
+    `;
+}
--- Frontend/src/js/main.js
+++ Frontend/src/js/main.js
@@ -2,6 +2,7 @@ import Nav from "./components/Nav"
 import apiActions from "./api/apiActions"
 import MainIngredient from "./components/MainIngredient"
 import MealType from "./components/MealType"
+import Recipe from "./components/Recipe"
 
 export default () => {
     pageBuild()
@@ -11,6 +12,7 @@ function pageBuild(){
     nav();
     mainingredientnav();
     MealTypenav();
+    Recipenav()
 }
 
 function nav(){
@@ -34,6 +36,16 @@ function MealTypenav(){
         apiActions.getRequest("https://localhost:44339/api/MealType", mealtype => {
             document.querySelector("#app").innerHTML = MealType(mealtype);
             console.log(mealtype);
+        });
+    });
+}
+
+function Recipenav(){
+    const navRecipe = document.querySelector("#Recipe");
+    navRecipe.addEventListener("click", function(){
+        apiActions.getRequest("https://localhost:44339/api/Recipe", recipe => {
+            document.querySelector("#app").innerHTML = Recipe(recipe);
+            console.log(recipe);
         });
     });
 }
