7cdd764390e4c01ceaed98878f600f49c67082af
Aaron Rock <aaronrk@mail.com>
2019-11-15 16:45:24 -0500

    begin css

--- Frontend/src/js/components/Nav.js
+++ Frontend/src/js/components/Nav.js
@@ -2,12 +2,18 @@ export default function Nav(){
     return `
     <nav>
         <div class = "row">
+            
+            <ul class = "main-nav" id= "buttonBox">
             <h2>Welcome to HealthyFoods</h2>
-            <h3>Here you can look at the Healthy food options we have!</h3>
-            <ul class = "main-nav">
-                <li><a id = "MainIngredient">Main Ingredient</a></li>
-                <li><a id = "MealType">Meal Type</a></li>
-                <li><a id = "Recipe">Recipe</a></li>
+            <br>
+            <h3>Here You Can Choose From The Healthy Food Options Below:</h3>
+            <br>
+                <ul><a id = "MainIngredient">Main Ingredient</a></ul>
+                <br>
+                <ul><a id = "MealType">Meal Type</a></ul>
+                <br>
+                <ul><a id = "Recipe">Recipe</a></ul>
+                <br>
             </ul>
         </div>
     </nav>
